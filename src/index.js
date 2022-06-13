d1229cbb85882fb2e4992c393a35329bf46e1df7
Damilola Balogun <balogundamilola.f@gmail.com>
2022-06-13 22:29:09 +0100

    my solution to she code mentee application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // complete the function
-function prime (num) {
+function prime(num) {
   // code goes here
+  for (var test = 2; test < num; test++) {
+    if (num % test === 0) return false;
+  }
+  return num > 1;
 }
 
-function solution (arg) {
+function solution(arg) {
   return prime(arg);
 }
 
-if (typeof require !== "undefined" && require.main === module) {
+if (typeof require !== 'undefined' && require.main === module) {
   if (process.argv.length <= 2) {
-    throw Error("Require argument");
+    throw Error('Require argument');
   }
 
   const arg = process.argv[2];
